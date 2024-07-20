0e362c1b17f56fcb96f9e42f5511ae74afbbf193
BrunoCastroTomaz <brunoworkjobs@gmail.com>
2024-07-19 23:29:36 -0300

    Commitando arquivo inicial CalculadoraIdade

---
+++ Python/CalculadoraIdade.py
@@ -0,0 +1,13 @@
+'''
+Autor: Bruno Castro Tomaz
+
+                    CALCULADORA IDADE EM SEGUNDOS
+
+    Este mini- projeto é uma calculadora que informa a sua idade aproximada em segundos.
+O programa lida apenas datas de 1º de janeiro de 1990 até o presente. Considera as seguintes infomações:
+            ** mês (1- 12)
+            ** dia (1 -31)
+            ** ano (4 dígitos)
+
+'''
+
